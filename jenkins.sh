4c33f292b0fbd6e21d42a472f4c298306cc43dab
wc-on-host198 <wc@wc.com>
2024-04-01 14:57:29 +0800

    onedev debug ci

--- jenkins.sh
+++ jenkins.sh
@@ -34,7 +34,7 @@ package_new_name=$package_base_name-$PBR_VERSION.tar.gz
 /bin/cp dist/$package_name $package_new_name
 
 IDENTITY=wc@192.168.1.4
-FILEDIR=/smb/4t/fileserver/jenkins/production-pve/$JOB_NAME
+FILEDIR=/smb/4t/fileserver/jenkins/production-pve/$PROJECT_NAME
 echo "FILEDIR is $FILEDIR"
 md5sum $package_new_name > $package_new_name.md5
 ssh $IDENTITY mkdir -p $FILEDIR/$BUILD_NUMBER
