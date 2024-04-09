f3d4ea1b8af863f6724e4dc1376a144387f7f428
wc-on-host056 <wc@wc.com>
2024-04-09 13:48:32 +0800

    add my doc to samba

--- cc_utils/__init__.py
+++ cc_utils/__init__.py
@@ -2,4 +2,10 @@ from .func import _
 
 AUTHOR_NAME = 'cc'
 AUTHOR_ZH_NAME = 'B站UP主吵吵博士'
+AUTHOR_GIFT_NAME = f'{AUTHOR_ZH_NAME}的赠礼'
 AIO_CONF_NAME = 'aio.conf'
+
+class Author:
+    name = 'cc'
+    zh_name = 'B站UP主吵吵博士'
+    gift_name = 'B站UP主吵吵博士的赠礼'
--- hostadmin/business/service.py
+++ hostadmin/business/service.py
@@ -11,7 +11,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from oslo_config import cfg
 from crontab import CronTab
 
-from cc_utils import execute, func, file, AUTHOR_NAME, AIO_CONF_NAME, network, AUTHOR_ZH_NAME
+from cc_utils import execute, func, file, AIO_CONF_NAME, network, Author
 from hostadmin.files import FilesDir
 from hostadmin.config import AUTHOR_SCRIPTS_DIR, CONF
 
@@ -55,8 +55,8 @@ class ServiceEndPoint(object):
         self.wireguard_conf_path = '/etc/wireguard/wg0.conf'
         self.wireguard_script_path = os.path.join(AUTHOR_SCRIPTS_DIR, self.wireguard_script)
         self.wireguard_params_path = '/etc/wireguard/params'
-        self.cc_doc_path = f'/usr/local/{AUTHOR_NAME}/doc'
-        self.pvetui_conf_path = f'/etc/{AUTHOR_NAME}/{AIO_CONF_NAME}'
+        self.cc_doc_path = f'/usr/local/{Author.name}/doc'
+        self.pvetui_conf_path = f'/etc/{Author.name}/{AIO_CONF_NAME}'
         self.create_local_alist_storage_py = 'create-local-alist-storage.py'
 
     def install_alist(self, ctxt):
@@ -98,14 +98,15 @@ class ServiceEndPoint(object):
             execute.completed(flag, f"delete old samba service samba user")
         flag, content = execute.execute_command(f'(echo {samba_user_password}; echo {samba_user_password}) | smbpasswd -s -a samba')
         execute.completed(flag, f"create samba service user=samba")
-        flag, content = execute.execute_command(f'cp -r /opt/{AUTHOR_NAME}/{AUTHOR_NAME}-aio-bin/gift/* {share_path}')
+        flag, content = execute.execute_command(f'cp -r /opt/{Author.name}/{Author.name}-aio-bin/gift/* {share_path}')
         execute.completed(flag, f"copy chaochao gift")
         md_file_names = [file_name for file_name in os.listdir(self.cc_doc_path) if '.md' in file_name]
-        pve_ip = network.get_main_ip_address()
+        pve_ip_cidr = network.get_main_ip_address()
+        pve_ip = func.get_string_split_list(pve_ip_cidr, split_flag='/')[0]
         for md_name in md_file_names:
-            content = file.read_file_content(os.path.join(self.cc_doc_path, file_path))
+            content = file.read_file_content(os.path.join(self.cc_doc_path, md_name), mode='r')
             content = content.replace("YOUR_PVE_ADDRESS", pve_ip)
-            file.write_file_content(f'{share_path}/{AUTHOR_ZH_NAME}的赠礼/md_name', content)
+            file.write_file_content(f'{share_path}/{Author.gift_name}/{md_name}', content)
         flag, content = execute.execute_command(f'chown -R samba:sambashare {share_path}')
         execute.completed(flag, f"chown share_path={share_path}")
         # 生效配置文件 重启smbd服务
@@ -122,11 +123,11 @@ class ServiceEndPoint(object):
     def create_alist_service(self, ctxt, admin_password):
         flag = execute.execute_command_in_popen(f'apt install net-tools -y')
         execute.completed(flag, f"apt install net-tools")
-        flag = execute.execute_command_in_popen(f'bash /usr/local/{AUTHOR_NAME}/scripts/install_alist.sh install')
+        flag = execute.execute_command_in_popen(f'bash /usr/local/{Author.name}/scripts/install_alist.sh install')
         execute.completed(flag, f"install alist")
         flag, content = execute.execute_command(f'sleep 2')
         execute.completed(flag, f"wait 2 seconds", content)
-        flag = execute.execute_command_in_popen(f'cd /opt/{AUTHOR_NAME}/alist && ./alist admin set {admin_password}')
+        flag = execute.execute_command_in_popen(f'cd /opt/{Author.name}/alist && ./alist admin set {admin_password}')
         execute.completed(flag, f"modify alist admin password")
         path = os.path.join(AUTHOR_SCRIPTS_DIR, self.create_local_alist_storage_py)
         flag, content = execute.execute_command(f'sleep 2')
@@ -178,7 +179,7 @@ class ServiceEndPoint(object):
             flag, content = execute.execute_command(f"rm -f /tmp/public_ip.txt")
             if flag == 0:
                 execute.completed(flag, f"delete old record")
-            flag, content = execute.execute_command(f'bash /usr/local/{AUTHOR_NAME}/scripts/report_public_ip_if_changed_robot.sh')
+            flag, content = execute.execute_command(f'bash /usr/local/{Author.name}/scripts/report_public_ip_if_changed_robot.sh')
             execute.completed(flag, f"start listen_public_ip_change_rebot", content)
             flag, content = execute.execute_command('systemctl restart cron', shell=False, timeout=10)
             execute.completed(flag, 'systemctl restart cron', content)
